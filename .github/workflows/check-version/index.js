ec24c6c6566d2e4d4fba7c30de5cbe574df3c560
Jair Almaraz <jair.almaraz@globant.com>
2022-08-07 18:57:39 -0500

    Action con js

--- .github/workflows/check-version/index.js
+++ .github/workflows/check-version/index.js
@@ -4,21 +4,9 @@ const fetch = require('node-fetch');
 const semver = require('semver');
 
 try {
-  console.log('esta entrando a la action');
-  console.log(github.context);
-  if (github.context.eventName !== 'pull_request') {
-    core.info('Skipping as it is not pull request');
-    return;
-  }
-
   const token = core.getInput('token');
   console.log(token);
   const headers = {};
-  if (token) {
-    core.info('Using specified token');
-    headers.Authorization = `token ${token}`;
-  }
-
   const baseSha = github.context.payload.pull_request.base.sha;
   const headSha = github.context.payload.pull_request.head.sha;
 
@@ -30,7 +18,8 @@ try {
     .then(res => res.version)
     .then(version => {
       const localVersion = require(`${ process.env.GITHUB_WORKSPACE }/package.json`).version;
-
+      core.info('Version que estoy enviando:', localVersion);
+      core.info('Version en el server:', version);
       if (!semver.valid(localVersion)) core.setFailed(`Current version '${ localVersion }' detected as invalid one`);
       if (!semver.gt(localVersion, version)) core.setFailed(`Version '${ localVersion }' wasn't detected as greater than '${ version }'`);
     })
