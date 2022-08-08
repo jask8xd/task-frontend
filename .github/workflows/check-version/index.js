a2411c3e6d5a0820f8e5b77c71c9cc9f1d2eb5eb
Jair Almaraz <jair.almaraz@globant.com>
2022-08-08 09:37:05 -0500

    update version

--- .github/workflows/check-version/index.js
+++ .github/workflows/check-version/index.js
@@ -1,14 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fetch = require('node-fetch');
-const semver = require('semver');
 
 try {
   const token = core.getInput('token');
-  console.log(token);
-  const headers = {};
+  const headers = {
+    Authorization: `token ${token}`
+  };
   const baseSha = github.context.payload.pull_request.base.sha;
-
   const baseUrl = `https://raw.githubusercontent.com/${ github.context.repo.owner }/${ github.context.repo.repo }/${ baseSha }/package.json`
 
   fetch(baseUrl, { headers })
@@ -16,11 +15,8 @@ try {
     .then(res => res.version)
     .then(version => {
       const localVersion = require(`${ process.env.GITHUB_WORKSPACE }/package.json`).version;
-      core.info(`Version que estoy enviando: ${localVersion} tipo: ${typeof localVersion }`);
-      core.info(`Version en el server: ${version} tipo: ${typeof version }`);
       const versionArr = version.split('.');
       const requiredVersion = `${versionArr[0]}.${versionArr[1]}.${Number(versionArr[2])+1}`
-      core.info(`Version requerida: ${requiredVersion}`);
       if(requiredVersion !== localVersion) core.setFailed(`Current version '${ localVersion }' should be the version '${ requiredVersion }'`);
     })
     .catch(core.setFailed);
